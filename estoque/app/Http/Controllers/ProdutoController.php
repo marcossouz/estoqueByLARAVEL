b3052e273153f398aede29128193b7e68c109820
mrcsz <marss@ic.ufal.br>
2018-02-23 17:43:22 -0300

    acesso ao banco de dados configurado e testado

--- estoque/app/Http/Controllers/ProdutoController.php
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,26 @@
 	
 	namespace estoque\Http\Controllers;
 
+	use Illuminate\Support\Facades\DB;
+
 	class ProdutoController extends Controller {
 		
 		public function lista() {
-			return '<h1>Listagem de produtos com laravel</h1>';
+
+			$produtos = DB::select('select * from produtos');
+
+			$html = '<h1>Listagem de produtos com Laravel</h1>';
+
+			$html .= '<ul>';
+
+			foreach($produtos as $p) {
+				$html .= 	'<li> Nome: ' . $p->nome . 
+						 	', Descrição: ' . $p->descricao . 
+						 	'</li>';
+			}
+
+			$html .= '</ul>';
+
+			return $html;
 		}
 	}
